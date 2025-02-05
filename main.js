d1163164aa47ea696881db95658a494f1f6674e9
Bennett Struttman <ben.struttman@gmail.com>
2025-02-05 10:33:28 -0600

    test

--- main.js
+++ main.js
@@ -14,14 +14,11 @@ document.body.style.overflow = "hidden";
 context.strokeStyle = "blue";
 context.strokeRect(100, 100, 100, 100);
 
-// Create a new image element 
 const img = document.createElement('img'); 
- 
-// Set the source and other attributes 
 img.src = './bakh.png'; // Specify the path to your image 
-img.alt = 'Description of the image'; // Alt text for accessibility 
-img.width = 300; // Optional: set width 
-img.height = 200; // Optional: set height
+// img.alt = 'Description of the image'; // Alt text for accessibility 
+// img.width = 300; // Optional: set width 
+// img.height = 200; // Optional: set height
 
 img.addEventListener("load", () => {
 	context.drawImage(img, 0, 0);
